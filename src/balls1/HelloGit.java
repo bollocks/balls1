ee27067fb63c56b47aee378f588089af1acacb59
bollocks <fitinferno@mac.com>
2014-07-22 19:35:26 +0100

    Update HelloGit.java
    hmmmmmmmmmmmmmm

--- src/balls1/HelloGit.java
+++ src/balls1/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
 
 	public static void main (String args[]) {
 		System.out.println("Balls to Bob");
-		System.out.println(".. who i secretly love");
+		System.out.println(".. who she secretly love, innit");
 	}
 	
 }
