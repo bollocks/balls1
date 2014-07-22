711ee53ae45b5f721300f8b5fa5f6eceef92d944
bollocks <fitinferno@mac.com>
2014-07-22 19:44:42 +0100

    i broke it good

--- src/balls1/HelloGit.java
+++ src/balls1/HelloGit.java
@@ -5,6 +5,8 @@ public class HelloGit {
 	public static void main (String args[]) {
 		System.out.println("Balls to Bob");
 		System.out.println(".. who i secretly love");
+
+THIS WON'T COMPILE!!! IDJIOT!!!
 	}
 	
 }
